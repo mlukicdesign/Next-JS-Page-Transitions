a2cd2c008fedb31a91d8ad280b329724a8f39b00
mlukicdesign <mlukicdesign@gmail.com>
2024-01-01 16:02:03 +0800

    Initial upload for future reference

---
+++ pages/services.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Page from '../components/page'
+
+const services = () => {
+  return (
+    <Page>
+        <h1>Services</h1>
+    </Page>
+  )
+}
+
+export default services
